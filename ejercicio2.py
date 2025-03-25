34b2af1e75ad10bd8280ecb45e724cc0c7c1cf55
Joaquin Juarez <joaquinignaciojuarez@gmail.com>
2025-03-25 07:57:20 -0300

    Se creo ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre ")
+apellido = input("Ahora ingrese su apellido ")
+universidad = input("Ingrese donde estudia ")
+
+print(f"Hola soy {nombre} {apellido}, estudiante de la {universidad}")
